2585c26c01bdfa5b5a8f5998fa55fefa1159fbb2
Deepak Rout <deepakrout@gmail.com>
2020-02-05 22:07:56 -0500

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA1Snw1TPLOkrEd668-uVXRODtrOAXQ5BA',
+    authDomain: 'orgshop-40f88.firebaseapp.com',
+    databaseURL: 'https://orgshop-40f88.firebaseio.com',
+    projectId: 'orgshop-40f88',
+    storageBucket: 'orgshop-40f88.appspot.com',
+    messagingSenderId: '580644531764',
+    appId: '1:580644531764:web:f316edba9b82b14b36e461',
+    measurementId: 'G-DV2V2YR6BW'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA1Snw1TPLOkrEd668-uVXRODtrOAXQ5BA',
+    authDomain: 'orgshop-40f88.firebaseapp.com',
+    databaseURL: 'https://orgshop-40f88.firebaseio.com',
+    projectId: 'orgshop-40f88',
+    storageBucket: 'orgshop-40f88.appspot.com',
+    messagingSenderId: '580644531764',
+    appId: '1:580644531764:web:f316edba9b82b14b36e461',
+    measurementId: 'G-DV2V2YR6BW'
+  }
 };
 
 /*
